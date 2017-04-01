c4a05413120f1e9c91d961905642ea073a600364
alex_023 <alex_023@126.com>
2017-04-01 11:15:58 +0800

    optimize counter
    user package atomic optimize counter.DecOne,counter.Current...

--- counter.go
+++ counter.go
@@ -2,47 +2,45 @@ package basekit
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 //Counter counter is a multi-thread safe counters
 type Counter struct {
 	mut     sync.Mutex
-	currNum int //当前数量
-	maxNum  int //最大数量
+	currNum int64 //当前数量
+	maxNum  int64 //最大数量
 }
 
 //AddOne 在原内部计数基础上，+1。
 func (c *Counter) AddOne() int {
 	c.mut.Lock()
-	defer c.mut.Unlock()
 
 	c.currNum++
 
 	if c.maxNum < c.currNum {
 		c.maxNum = c.currNum
 	}
+	c.mut.Unlock()
 
-	return c.currNum
+	return int(c.currNum)
 }
 
 //DecOne 在原内部计数基础上，-1。
 func (c *Counter) DecOne() int {
-	c.mut.Lock()
-	defer c.mut.Unlock()
 
-	c.currNum--
+	return int(atomic.AddInt64(&c.currNum, -1))
 
-	return c.currNum
 }
 
 //Current 获取当前内部计数结果。
 func (c *Counter) Current() int {
-	return c.currNum
+	return int(atomic.LoadInt64(&c.currNum))
 }
 
 //MaxNum 计数器生存周期内，最大的计数。
 func (c *Counter) MaxNum() int {
-	return c.maxNum
+	return int(atomic.LoadInt64(&c.maxNum))
 }
 
 //NewCounter counter constructor
